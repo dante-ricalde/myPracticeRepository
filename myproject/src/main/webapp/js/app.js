3437153951231b94bb6e6b147eda5246e42da106
Dante Raphael Ricalde Delgado <dante.ricalde.delgado@gmail.com>
2017-02-25 15:12:33 +0000

    components in angular1

--- myproject/src/main/webapp/js/app.js
+++ myproject/src/main/webapp/js/app.js
@@ -1,1 +1,29 @@
 angular.module('app', ['components'])
+
+.controller('BeerCounter', function ($scope, $locale, $rootScope) {
+	$scope.beers = [0, 1, 2, 3, 4, 5, 6];
+
+	$scope.init = function(locale) {
+		$scope.setBeerForms(locale);
+	};
+
+	$scope.setBeerForms = function (locale) {
+		if (locale == 'en-us') {
+			$scope.beerForms = {
+				0: 'no beers',
+				one: '{} beer',
+				other: '{} beers'
+			};
+		} else {
+			$scope.beerForms = {
+				0: 'žiadne pivo',
+				one: '{} pivo',
+				few: '{} pivá',
+				other: '{} pív'
+			};
+		}
+	};
+
+	$scope.setBeerForms($locale.id);
+	
+});
--- myproject/src/main/webapp/js/components.js
+++ myproject/src/main/webapp/js/components.js
@@ -1,1 +1,57 @@
 angular.module('components', [])
+	.directive('tabs', function () {
+		return {
+			restrict: 'E',
+			transclude: true,
+			scope: {},
+			controller: function($scope, $element) {
+				console.log('executing tabs controller ...');
+				var panes = $scope.panes = [];
+
+				$scope.select = function(pane) {
+					console.log('Selecting pane');
+					angular.forEach(panes, function(pane) {
+						pane.selected = false;
+					});
+					pane.selected = true;
+				};
+				this.addPane = function(pane) {
+					console.log('Adding pane ...');
+					if (panes.length == 0) $scope.select(pane);
+					panes.push(pane);
+				};
+			},
+			template: `
+				<div class="tabbable">
+					<ul class="nav nav-tabs">
+						<li ng-repeat="pane in panes" ng-class="{active:pane.selected}">
+							<a href="" ng-click="select(pane)">{{pane.title}}</a>
+						</li>
+					</ul>
+					<div class="tab-content" ng-transclude></div>
+				</div>
+			`,
+			replace: true
+		};
+	}).
+	directive('pane', function($locale){
+		return {
+			require: '^tabs',
+			restrict: 'E',
+			transclude: true,
+			scope: { title: '@', locale: '@' },
+			link: function(scope, element, attrs, tabsController) {
+				$locale.id=scope.locale;
+				console.log('locale param set in pane element: ' + scope.locale);
+				tabsController.addPane(scope);
+			},
+			controller: function($scope, $element) {
+				console.log('executing pane controller ...');
+			},
+			template: `
+				<div class="tab-pane" ng-class="{active: selected}" ng-transclude>
+				</div>
+			`,
+			replace: true
+		};
+	});
