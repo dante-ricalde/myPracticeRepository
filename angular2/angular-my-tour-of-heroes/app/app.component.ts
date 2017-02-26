d6595dfb8c742a3322bddf7cd567733c4e3a845a
Dante Raphael Ricalde Delgado <dante.ricalde.delgado@gmail.com>
2017-02-26 12:29:30 +0000

    styles completed

--- angular2/angular-my-tour-of-heroes/app/app.component.ts
+++ angular2/angular-my-tour-of-heroes/app/app.component.ts
@@ -2,16 +2,18 @@ import { Component } from '@angular/core';
 
 // This AppComponent is a Router Component because (The AppComponent is now attached to a router and displaying routed views).
 @Component({
+	moduleId: module.id,
 	selector: 'my-app',
 	template: `
 		<h1>{{title}}</h1>
 		<nav>
-			<a routerLink="/dashboard">Dashboard</a>
-			<a routerLink="/heroes">Heroes</a>
-			<a routerLink="/detail/12">Narco</a>
+			<a routerLink="/dashboard" routerLinkActive="active">Dashboard</a>
+			<a routerLink="/heroes" routerLinkActive="active">Heroes</a>
+			<a routerLink="/detail/12" routerLinkActive="active">Narco</a>
 		</nav>
 		<router-outlet></router-outlet>
-	`
+	`,
+	styleUrls: [ './app.component.css' ]
 })
 export class AppComponent {
 	title = 'Tour of Heroes';
